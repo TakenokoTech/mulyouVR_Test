1c5f532ee49938afb023365e52392263d899922d
Takenoko <takenoko.tech@gmail.com>
2019-09-06 02:14:48 +0900

    fix position

--- src/vr/scene/MainScene.tsx
+++ src/vr/scene/MainScene.tsx
@@ -1,3 +1,4 @@
+import * as dat from 'dat.gui';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as THREE from 'three';
@@ -33,6 +34,7 @@ export default class MainScene extends BaseThreeScene<MainSceneProps, MainSceneS
             this.camera.position.z = 50;
         }
 
+        this.makeStaticGui();
         return s;
     }
 
@@ -68,74 +70,116 @@ export default class MainScene extends BaseThreeScene<MainSceneProps, MainSceneS
             this.scene.add(ambientLight);
         }
         {
-            const allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
-            const dom = (
-                <div style={{ width: 1920 / 2 + 'px', textAlign: 'center' }}>
-                    <iframe width={1920 / 4} height={1080 / 4} src="https://www.youtube.com/embed/Wtn7RqAH5S8" allow={allow} />
-                    <iframe width={1920 / 4} height={1080 / 4} src="https://www.youtube.com/embed/Wtn7RqAH5S8" allow={allow} />
-                </div>
-            );
-            var element = document.createElement('div') as HTMLDivElement;
-            element.id = '__youtube';
-            ReactDOM.render(dom, element);
-            const object = new Element3DObject(element);
-            object.scale.set(0.3, 0.3, 0.3);
-            object.position.copy(new THREE.Vector3(0, 0, -300));
+            const videoId = 'EnN0r8G9oSM';
+            const sheta = 0 * THREE.Math.DEG2RAD;
+            const x = +300 * Math.cos(90 * (Math.PI / 180));
+            const z = -300 * Math.sin(90 * (Math.PI / 180));
+            const object = this.makeYoutubeObj(videoId, new THREE.Vector3(x, 0, z), new THREE.Euler(0, sheta, 0));
             this.scene.add(object);
+            this.hierarchy[`youtube_${videoId}`] = [object];
         }
         {
-            const allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
-            const dom = (
-                <div style={{ width: 1920 / 2 + 'px', textAlign: 'center' }}>
-                    <iframe width={1920 / 4} height={1080 / 4} src="https://www.youtube.com/embed/Wtn7RqAH5S8" allow={allow} />
-                    <iframe width={1920 / 4} height={1080 / 4} src="https://www.youtube.com/embed/Wtn7RqAH5S8" allow={allow} />
-                </div>
-            );
-            var element = document.createElement('div') as HTMLDivElement;
-            element.id = '__youtube1';
-            ReactDOM.render(dom, element);
-            const object = new Element3DObject(element);
-            object.scale.set(0.3, 0.3, 0.3);
-            object.position.copy(new THREE.Vector3(250, 0, -200));
-            object.rotation.copy(new THREE.Euler(0, -45 * THREE.Math.DEG2RAD, 0));
+            const videoId = 'v1fSB6C_Ps4';
+            const sheta = -30 * THREE.Math.DEG2RAD;
+            const x = +300 * Math.cos(60 * (Math.PI / 180));
+            const z = -300 * Math.sin(60 * (Math.PI / 180));
+            const object = this.makeYoutubeObj(videoId, new THREE.Vector3(x, 0, z), new THREE.Euler(0, sheta, 0));
             this.scene.add(object);
-            this.hierarchy[element.id] = [object];
+            this.hierarchy[`youtube_${videoId}`] = [object];
+        }
+        {
+            const videoId = 'LatVcl7TkRo';
+            const sheta = -60 * THREE.Math.DEG2RAD;
+            const x = +300 * Math.cos(30 * (Math.PI / 180));
+            const z = -300 * Math.sin(30 * (Math.PI / 180));
+            const object = this.makeYoutubeObj(videoId, new THREE.Vector3(x, 0, z), new THREE.Euler(0, sheta, 0));
+            this.scene.add(object);
+            this.hierarchy[`youtube_${videoId}`] = [object];
+        }
+
+        {
+            const videoId = 'N1A6iSEFqDA';
+            const sheta = +30 * THREE.Math.DEG2RAD;
+            const x = -300 * Math.cos(-60 * (Math.PI / 180));
+            const z = +300 * Math.sin(-60 * (Math.PI / 180));
+            const object = this.makeYoutubeObj(videoId, new THREE.Vector3(x, 0, z), new THREE.Euler(0, sheta, 0));
+            this.scene.add(object);
+            this.hierarchy[`youtube_${videoId}`] = [object];
+        }
+        {
+            const videoId = 'BcM4jKW9pRQ';
+            const sheta = +60 * THREE.Math.DEG2RAD;
+            const x = -300 * Math.cos(-30 * (Math.PI / 180));
+            const z = +300 * Math.sin(-30 * (Math.PI / 180));
+            const object = this.makeYoutubeObj(videoId, new THREE.Vector3(x, 0, z), new THREE.Euler(0, sheta, 0));
+            this.scene.add(object);
+            this.hierarchy[`youtube_${videoId}`] = [object];
         }
     };
 
-    /*
-    makeGui = () => {
+    makeYoutubeObj = (videoId: string, vec: THREE.Vector3, euler: THREE.Euler): Element3DObject => {
+        const allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+        const [width, height] = [500, (500 / 16) * 9];
+        const dom = (
+            <div style={{ width: width + 'px', textAlign: 'center' }}>
+                <iframe width={width} height={height} src={`https://www.youtube.com/embed/${videoId}`} allow={allow} />
+            </div>
+        );
+        const element = document.createElement('div') as HTMLDivElement;
+        element.id = `youtube_${videoId}`;
+        ReactDOM.render(dom, element);
+        const object = new Element3DObject(element);
+        object.scale.set(0.3, 0.3, 0.3);
+        object.position.copy(vec);
+        object.rotation.copy(euler);
+        return object;
+    };
+
+    makeGui = () => {};
+
+    datgui: dat.GUI | null = null;
+    makeStaticGui = () => {
         // super.makeGui();
-        const gui = new GUIVR.create('OrbitControls');
-        GUIVR.enableMouse(this.camera);
-        gui.position.set(0, 20, 0);
-        gui.scale.set(30, 30, 30);
+        // const gui = new GUIVR.create('OrbitControls');
+        // if (this.datgui)
+        return;
+        this.datgui = new dat.GUI();
+        //GUIVR.enableMouse(this.camera);
+        //gui.position.set(0, 20, 0);
+        //gui.scale.set(30, 30, 30);
         // gui.visible = this.state.visibleGui || false;
-        this.scene.add(gui);
-        gui.add(this.hierarchy['__youtube1'][0].position, 'x', -300, 300)
+        //this.scene.add(gui);
+
+        const videoId = 'v1fSB6C_Ps4';
+        this.datgui
+            .add(this.hierarchy[`youtube_${videoId}`][0].position, 'x', -300, 300)
             .step(0.01)
             .name('Camera Position X')
             .listen();
-        gui.add(this.hierarchy['__youtube1'][0].position, 'y', -300, 300)
+        this.datgui
+            .add(this.hierarchy[`youtube_${videoId}`][0].position, 'y', -300, 300)
             .step(0.01)
             .name('Camera Position Y')
             .listen();
-        gui.add(this.hierarchy['__youtube1'][0].position, 'z', -300, 300)
+        this.datgui
+            .add(this.hierarchy[`youtube_${videoId}`][0].position, 'z', -300, 300)
             .step(0.01)
             .name('Camera Position Z')
             .listen();
-        gui.add(this.hierarchy['__youtube1'][0].rotation, 'x', -Math.PI, Math.PI)
+        this.datgui
+            .add(this.hierarchy[`youtube_${videoId}`][0].rotation, 'x', -Math.PI, Math.PI)
             .step(0.01)
             .name('Camera Rotation X')
             .listen();
-        gui.add(this.hierarchy['__youtube1'][0].rotation, 'y', -Math.PI, Math.PI)
+        this.datgui
+            .add(this.hierarchy[`youtube_${videoId}`][0].rotation, 'y', -Math.PI, Math.PI)
             .step(0.01)
             .name('Camera Rotation Y')
             .listen();
-        gui.add(this.hierarchy['__youtube1'][0].rotation, 'z', -Math.PI, Math.PI)
+        this.datgui
+            .add(this.hierarchy[`youtube_${videoId}`][0].rotation, 'z', -Math.PI, Math.PI)
             .step(0.01)
             .name('Camera Rotation Z')
             .listen();
     };
-    */
 }
